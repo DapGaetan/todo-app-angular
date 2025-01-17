4a5faac6f747b53b7e2d3922e03e0ea19adf7324
Gaëtan <dapvril.gaetan@gmail.com>
2025-01-17 16:48:00 +0100

    feat: :sparkles: stock the task in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 export interface TodoItem{
   id:number;
@@ -14,8 +12,30 @@ export interface TodoItem{
   completed:boolean;
 }
 
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule, MatListModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+
 export class AppComponent {
   todoList : TodoItem [] = [];
 
   newTask:string = ''
+
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem : TodoItem= {
+        id : Date.now(),
+        task : this.newTask,
+        completed : false
+      }
+      this.todoList.push(newTodoItem)
+      console.log(this.todoList)
+
+      this.newTask = ''
+    }
+  }
 }
